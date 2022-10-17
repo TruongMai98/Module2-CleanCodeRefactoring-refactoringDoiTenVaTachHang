a819d0ef8bdc88a86817a8c2fef0d7bafbd71fce
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-17 10:50:17 +0700

    [Thực hành] Refactoring - đổi tên biến và tách hằng

---
+++ src/CalculatorTest.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        int firstOperand = 1;
+        int secondOperand = 3;
+        char operator = '*';
+        System.out.println(Calculator.calculate(firstOperand,secondOperand,operator));
+
+    }
+
+
+
+}
